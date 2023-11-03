51b45a446ed33e6d75f7e7fec8055262aa5389a2
alvmiller <134068320+alvmiller@users.noreply.github.com>
2023-11-03 14:17:33 +0200

    Update vector00s_angle.h

--- vector00s_angle.h
+++ vector00s_angle.h
@@ -21,33 +21,6 @@
    reset; gcc -Werror vector00s_angle.c main.c -lm -o main; ./main
  */
 
-/*
-   https://stackoverflow.com/questions/21483999/using-atan2-to-find-angle-between-two-vectors
-   https://wumbo.net/formulas/angle-between-two-vectors-2d/
-   https://gamedev.stackexchange.com/questions/69649/using-atan2-to-calculate-angle-between-two-vectors
-   https://wumbo.net/functions/arc-tangent-2/
-   https://www.programiz.com/c-programming/library-function/math.h/atan2
-   https://www.tutorialspoint.com/c_standard_library/c_function_atan2.htm
-   https://en.cppreference.com/w/c/numeric/math/atan2
-   https://en.wikipedia.org/wiki/Euclidean_vector
-   https://en.wikipedia.org/wiki/Coordinate_vector
-   https://courses.lumenlearning.com/suny-osuniversityphysics/chapter/2-2-coordinate-systems-and-components-of-a-vector/
-   https://www.programiz.com/c-programming/library-function/math.h/atan2
-   https://www.tutorialspoint.com/c_standard_library/c_function_atan2.htm
-   https://www.geeksforgeeks.org/function-pointer-in-c/
-   https://www.w3schools.com/c/c_enums.php
-   https://stackoverflow.com/questions/141720/how-do-you-compare-structs-for-equality-in-c
-   https://www.geeksforgeeks.org/c-exit-abort-and-assert-functions/
-   https://gcc.gnu.org/onlinedocs/gcc/Integer-Overflow-Builtins.html
-   https://www.tutorialspoint.com/c_standard_library/c_function_abs.htm
-   https://gcc.gnu.org/onlinedocs/gcc/Typeof.html
-   https://en.wikipedia.org/wiki/Assert.h
-   https://copyprogramming.com/howto/how-to-compare-two-types-in-c#how-to-compare-data-in-c
-   https://stackoverflow.com/questions/5989191/compare-two-floats
-   https://www.geeksforgeeks.org/comparison-float-value-c/
-   https://how-to.fandom.com/wiki/Howto_compare_floating_point_numbers_in_the_C_programming_language
- */
-
 /* @TODO: Check that both vectors are in the same line */
 
 typedef enum angle_type_t {
